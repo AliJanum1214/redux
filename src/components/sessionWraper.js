0a210df3999e6f8db78f6d750ea783849f994aca
AliJanum1214 <shampi.goli@gmail.com>
2025-05-12 17:45:27 +0500

    commit

--- src/components/sessionWraper.js
+++ src/components/sessionWraper.js
@@ -1,7 +1,7 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
 import { Provider } from "react-redux";
-import { store } from "@/redux/Store";
+import { store } from "@/redux/store";
 
 const ClientProviders = ({ children }) => {
   return (
